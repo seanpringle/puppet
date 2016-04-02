99d76ac95020a9618aa766f2fe1e0766d1d57537
Sean Pringle <sean.pringle@gmail.com>
2016-04-02 20:22:45 +1000

    stp900-dev

--- site.pp
+++ site.pp
@@ -2,4 +2,14 @@
 node /./
 {
   require base
+
+  class { 'base::postgresql':
+    version => '9.5',
+    sources => [ '10.100.53.0/24' ],
+  }
+
+  postgresql::server::db { 'test':
+    user     => 'test',
+    password => postgresql_password('test', 'test'),
+  }
 }
