88c94f8949da0a82acf3dcfaea37a87c18517c9d
Sean Pringle <sean.pringle@gmail.com>
2016-04-02 22:29:36 +1000

    wip

--- site.pp
+++ site.pp
@@ -3,6 +3,14 @@ node /./
 {
   require base
 
+  firewall { '200 allow postgres':
+    chain  => 'INPUT',
+    proto  => 'tcp',
+    dport  => '5432',
+    source => '10.100.53.0/24',
+    action => 'accept',
+  }
+
   class { 'base::postgresql':
     version => '9.5',
     sources => [ '10.100.53.0/24' ],
