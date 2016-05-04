3d4c3917a88a377c6043567e7972c40cda4192f2
Sean Pringle <sean.pringle@gmail.com>
2016-05-04 11:20:16 +1000

    bittorrent

--- site.pp
+++ site.pp
@@ -2,4 +2,19 @@
 node /./
 {
   require base
+
+  firewall { '100 allow bitorrent':
+    chain  => 'INPUT',
+    proto  => 'tcp',
+    dport  => '6881-6889',
+    action => 'accept',
+  }
+
+  $packages = [
+    'rtorrent',
+  ]
+
+  package { $packages:
+    ensure => present,
+  }
 }
