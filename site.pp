2c576d52a86de61c55009b4420e77b930d84d8ea
Sean Pringle <sean.pringle@gmail.com>
2016-05-13 19:45:40 +1000

    oneblueshow.net

--- site.pp
+++ site.pp
@@ -2,4 +2,13 @@
 node /./
 {
   require base
+
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  apache::vhost { 'oneblueshoe.net':
+    port    => '80',
+    docroot => '/var/www/oneblueshoe.net',
+  }
 }
